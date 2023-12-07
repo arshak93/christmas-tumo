1cdfce9391688c25f71d0003dac7d2f977f8fd12
arshak93 <arshak@aukilabs.com>
2023-12-07 15:21:56 +0400

    Lesson 4

--- Assets/Scripts/OrnamentSpawner.cs
+++ Assets/Scripts/OrnamentSpawner.cs
@@ -1,9 +1,11 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class OrnamentSpawner : MonoBehaviour
 {
+    [SerializeField] private Camera mainCamera;
+    [SerializeField] private List<Ornament> ornamentPrefabs;
+
     // Start is called before the first frame update
     void Start()
     {
