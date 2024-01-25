3084c12791e8c3a4af88c93835df41f6ead48f0f
arshak93 <>
2024-01-26 00:10:50 +0800

    cleanup

--- Assets/Scripts/Multiplayer/Database.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
-using System;
-
-[Serializable]
-public class Database
-{
-    public List<OrnamentData> xaxalinqer = new List<OrnamentData>();
-}
